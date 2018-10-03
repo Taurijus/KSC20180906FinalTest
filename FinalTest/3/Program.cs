0b72cd503fbef9c832a4ba012e5dd63274e3d6ca
Linas Grabnickas <linas.grabnickas@gmail.com>
2018-10-03 12:51:34 +0300

    Uzduotis 3

--- FinalTest/3/Program.cs
+++ FinalTest/3/Program.cs
@@ -8,8 +8,45 @@ namespace _3
 {
   class Program
   {
+    static List<int> gang = new List<int>();
     static void Main(string[] args)
     {
+      Console.WriteLine("Imput:");
+      int count = int.Parse(Console.ReadLine());
+      Console.WriteLine("Output:");
+      if (count > 0)
+        for (int i = 0; i < count; i++)
+          gang.Add(i + 1);
+      KillNextAfter(gang,1);
+
+      Console.WriteLine("Kelinta romėna iš kairės kirsti? - ");
+      int nextAfter = int.Parse(Console.ReadLine());
+      Console.WriteLine("Output:");
+      gang = new List<int>();
+      if (count > 0)
+        for (int i = 0; i < count; i++)
+          gang.Add(i + 1);
+      KillNextAfter(gang, nextAfter);
+      Console.ReadLine();
+    }
+
+    static void KillNextAfter(List<int> gang, int NextAfter)
+    {
+      if (gang.Count > 1)
+      {
+        //int index = 0;
+        //for (int i = 0; i < NextAfter; i++)
+        //{
+        //  if (index == gang.Count - 1) index = 0;
+        //  else index++;
+        //}
+        //Pasiliksiu prie matematinio ir rekursinio mix sprendimo
+        int index = NextAfter - NextAfter / gang.Count * gang.Count;
+        if (index > 0) gang.AddRange(gang.Where((x, i) => i < index).ToList());
+        gang.RemoveRange(0, index+1);
+        KillNextAfter(gang, NextAfter);
+      }
+      else Console.WriteLine(gang[0]);
     }
   }
 }
